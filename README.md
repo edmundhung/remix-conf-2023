b141c1ce9d33b82e6bb86d5a83e420a671fd3ec6
Edmund Hung <me@edmund.dev>
2023-04-08 15:12:33 +0200

    setup mdx deck

--- README.md
+++ README.md
@@ -1,22 +1,19 @@
-# Welcome to Remix!
+# Remix Conf 2023 - Remxing Constraint Validation
 
-- [Remix Docs](https://remix.run/docs)
+This slide was bootstrapped with [MDX Deck](https://github.com/jxnblk/mdx-deck) and [Code Surfer](https://codesurfer.pomb.us/). All examples are served from a Remix app.
 
 ## Development
 
-You will be utilizing Wrangler for local development to emulate the Cloudflare runtime. This is already wired up in your package.json as the `dev` script:
+To run the presentation deck in development mode:
 
 ```sh
-# start the remix dev server and wrangler
 npm run dev
 ```
 
-Open up [http://127.0.0.1:8788](http://127.0.0.1:8788) and you should be ready to go!
+## Build
 
-## Deployment
+To build the presentation deck:
 
-Cloudflare Pages are currently only deployable through their Git provider integrations.
-
-If you don't already have an account, then [create a Cloudflare account here](https://dash.cloudflare.com/sign-up/pages) and after verifying your email address with Cloudflare, go to your dashboard and follow the [Cloudflare Pages deployment guide](https://developers.cloudflare.com/pages/framework-guides/deploy-anything).
-
-Configure the "Build command" should be set to `npm run build`, and the "Build output directory" should be set to `public`.
+```sh
+npm run build
+```
---
+++ deck/06.Action.jsx
@@ -0,0 +1,75 @@
+import { Form } from '@remix-run/react';
+import { useState } from 'react';
+import { login } from "~/auth.server";
+
+function formatValidity(input) {
+  if (input.validity.valueMissing) {
+    return "The field is required";
+  }
+
+  if (input.validity.typeMismatch || input.validity.patternMismatch) {
+    return "The value is invalid";
+  }
+
+  return "";
+}
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  const value = Object.fromEntries(formData);
+  
+  return await login(value);
+}
+
+export default function LoginForm() {
+  const [error, setError] = useState({});
+
+  return (
+    <Form
+      method="post"
+      onInvalidCapture={(event) => {
+        const input = event.target;
+  
+        setError((error) => ({
+          ...error,
+          [input.name]: formatValidity(input),
+        }));
+
+        event.preventDefault();
+      }}
+      onSubmit={(event) => {
+        const form = event.currentTarget;
+
+        setError({});
+
+        if (!form.reportValidity()) {
+          event.preventDefault();
+        }
+      }}
+      noValidate
+    >
+      <div>
+        <label>Email</label>
+        <input
+          className={error.email ? 'error' : ''}
+          name="email"
+          type="email"
+          required
+          pattern="[^@]+@[A-Za-z0-9]+.[A-Za-z0-9]+"
+        />
+        <div>{error.email}</div>
+      </div>
+      <div>
+        <label>Password</label>
+        <input
+          className={error.password ? 'error' : ''}
+          name="password"
+          type="password"
+          required
+        />
+        <div>{error.password}</div>
+      </div>
+      <button>Login</button>
+    </Form>
+  );
+}
